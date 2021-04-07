0f4e44ef6be59099c70b5a72e7adc7e2afa786c4
Ben Hurworth <ben.hurworth@gmail.com>
2021-04-07 10:41:41 +0100

    feat: section 1 bloodsport images

--- client/src/components/Navigation/Navbar.js
+++ client/src/components/Navigation/Navbar.js
@@ -4,12 +4,12 @@ export function Navbar() {
     return (
         <div>
         <header className = "nav-header">
-        <h2 className = "logo"><a href = "index.html">CS:GO STAT TRACKER</a></h2>
+        <h2 className = "logo"><a href = "index.html">STAT TRACKER</a></h2>
         <nav>
         <ul className = "nav-links">
-                <li><a href = "#"  className = "links">Projects</a></li>
-                <li><a href = "#"  className = "links">Photography</a></li>
-                <li><a href = "#"  className = "links">About</a></li>
+                <li><a href = "#"  className = "links">CSGO</a></li>
+                <li><a href = "#"  className = "links">Rocket League</a></li>
+                <li><a href = "#"  className = "links">Warzone</a></li>
             </ul> 
         </nav>
         {/* <a href = "#section4"><button>Contact</button></a>  */}
---
+++ client/src/components/Section1/Section1.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import bloodsportImage from "../../images/Bloodsport2.png"
+
+export function Section1() {
+    return (
+        <>
+      <section className = "section1">
+        <img src={bloodsportImage} alt="" className = "section1_img"></img>
+        <h1>CS:GO STAT TRACKER</h1>
+        <h5>Discover your true skill level</h5>
+        <div className="custom-shape-divider-bottom-1615671062">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill"></path>
+            </svg>
+        </div>  
+    </section>
+
+        </>
+           
+    )
+}
+
+
+    
