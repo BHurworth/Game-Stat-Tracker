9df084e19ca595847838e313beff14cf22bcf2bc
Ben Hurworth <ben.hurworth@gmail.com>
2021-04-09 16:47:59 +0100

    feat: add React Router functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
 //import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { Data } from "./components/Data/Data.js";
-import { Navbar } from "./components/Navigation/Navbar.js";
-import {Section1} from "./components/Section1/Section1.js"
-import {SteamIdForm} from "./components/Data/SteamIdForm.js";
+//import axios from "axios";
+//import { Data } from "./components/Data/Data.js";
 
-import useStyles from "./styles";
+import csgoStat from "./pages/csgoStat.js";
+import index from "./pages/index.js";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect,
+} from "react-router-dom";
+
+
 import "./App.css";
 
-function App() {
 
+
+
+function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
-      <Section1></Section1>
-      <SteamIdForm></SteamIdForm>
-     
-    </div>
+   <Router>
+     <Route path="/" component={index} />
+    </Router>
   );
 }
 
