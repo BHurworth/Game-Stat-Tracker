4ab63e55467ad2ec1aa752a69004821e4d8047d3
Ben Hurworth <ben.hurworth@gmail.com>
2021-04-09 16:49:00 +0100

    fix: update list of game names

--- client/src/components/Navigation/Navbar.js
+++ client/src/components/Navigation/Navbar.js
@@ -7,9 +7,9 @@ export function Navbar() {
         <h2 className = "logo"><a href = "index.html">STAT TRACKER</a></h2>
         <nav>
         <ul className = "nav-links">
-                <li><a href = "#"  className = "links">CSGO</a></li>
                 <li><a href = "#"  className = "links">Rocket League</a></li>
-                <li><a href = "#"  className = "links">Warzone</a></li>
+                <li><a href = "#"  className = "links">CS:GO</a></li>
+                <li><a href = "#"  className = "links">League of Legends</a></li>
             </ul> 
         </nav>
         {/* <a href = "#section4"><button>Contact</button></a>  */}
