5b95204c6d11a3b7c6c7fe9c6d6f71daa751347f
Ben Hurworth <ben.hurworth@gmail.com>
2021-04-10 16:05:50 +0100

    feat: add react links for eachgame in navbar

--- client/src/components/Navigation/Navbar.js
+++ client/src/components/Navigation/Navbar.js
@@ -1,21 +1,39 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect,
+  BrowserRouter,
+} from "react-router-dom";
 
 export function Navbar() {
     return (
-        <div>
-        <header className = "nav-header">
-        <h2 className = "logo"><a href = "index.html">STAT TRACKER</a></h2>
-        <nav>
-        <ul className = "nav-links">
-                <li><a href = "#"  className = "links">Rocket League</a></li>
-                <li><a href = "#"  className = "links">CS:GO</a></li>
-                <li><a href = "#"  className = "links">League of Legends</a></li>
-            </ul> 
-        </nav>
-        {/* <a href = "#section4"><button>Contact</button></a>  */}
-        </header> 
-        </div>
-    )
+      <div>
+        <header className="nav-header">
+          <Link to="/" className="reactNavLink">
+            <h2 className="logo">STAT TRACKER</h2>
+          </Link>
+          <nav>
+            <ul className="nav-links">
+              <Link to="/rl" className="reactNavLink">
+                <li className="links">Rocket League</li>
+              </Link>
+
+              <Link to="/csgo" className="reactNavLink">
+                <li className="links">CS:GO</li>
+              </Link>
+
+              <Link to="/lol" className="reactNavLink">
+                <li className="links">League of Legends</li>
+              </Link>
+            </ul>
+          </nav>
+          {/* <a href = "#section4"><button>Contact</button></a>  */}
+        </header>
+      </div>
+    );
 }
 
 
