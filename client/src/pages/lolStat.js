d3ecf0c85fa833584d6d7c15fd03d2e7fe0614e6
Ben Hurworth <ben.hurworth@gmail.com>
2021-04-10 16:06:27 +0100

    feat: initialise component and add basic h1

---
+++ client/src/pages/lolStat.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function lolStat() {
+  return (
+    <div>
+      <h1>Lol stats</h1>
+    </div>
+  );
+}
+
+export default lolStat;
---
+++ client/src/pages/rlStat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function rlStat() {
+    return (
+        <div>
+            <h1>Rocket League stats</h1>
+        </div>
+    )
+}
+
+export default rlStat
