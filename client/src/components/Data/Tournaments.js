1cfd77615f998eaa9474703fa48c6630a2554dee
Ben Hurworth <ben.hurworth@gmail.com>
2021-04-14 16:04:23 +0100

    feat: add basic h1

---
+++ client/src/components/Data/Tournaments.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Tournaments(props) {
+    return (
+        <div className = {props.className}>
+            <h1>Tournaments</h1>
+            <h2>weawdas</h2>
+        </div>
+    )
+}
+
+export default Tournaments
