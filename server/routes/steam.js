4bc175cf74e2777fdf192c6ec8900eaaf587c9c0
Ben Hurworth <ben.hurworth@gmail.com>
2021-04-14 17:02:47 +0100

    fix: remove unnecssary commented out code

--- server/routes/steam.js
+++ server/routes/steam.js
@@ -5,8 +5,4 @@ const router = express.Router();
 
 router.get("/:id", getSteamData);
 
-//router.get("/", getStudents);
-//router.post("/", createStudent);
-//router.delete('/:id', deleteStudent);
-
 export default router;
