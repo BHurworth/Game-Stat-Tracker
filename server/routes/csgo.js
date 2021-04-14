2b178a808c0dfa26d1379e11fcd53b513de5244f
Ben Hurworth <ben.hurworth@gmail.com>
2021-04-14 17:34:08 +0100

    fix: change order of routes to fix bug

--- server/routes/csgo.js
+++ server/routes/csgo.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { getCsgoStats } from "../controllers/csgo.js";
+import {getCsgoTournaments} from "../controllers/csgoTournaments.js"
 
 const router = express.Router();
+
+router.get("/tournaments", getCsgoTournaments);
 
 router.get("/:id", getCsgoStats);
 
