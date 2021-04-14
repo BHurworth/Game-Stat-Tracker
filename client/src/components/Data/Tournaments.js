0e08acc2a645d2e6eadc86042d6e259581b46d35
Ben Hurworth <ben.hurworth@gmail.com>
2021-04-14 22:21:17 +0100

    feat: add card list container for styles and tomap data for each card instantiation

--- client/src/components/Data/Tournaments.js
+++ client/src/components/Data/Tournaments.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import Card from "./TournamentDataCards.js";
 
-function Tournaments(props) {
-    return (
-        <div id="tourn-container" className = {props.className}>
-            <h1 className = "tourn-title">Tournaments</h1>
+function TournamentsData(props) {
+  console.log(props.data);
 
+
+  return (
+    <div id="tourn-container" className={props.className}>
+      <h1 className="tourn-title">Tournaments</h1>
+        <div id="tournament-cards">
+            {props.data.map((data) => {
+            return (
+                <Card
+                // img={data.league.image_url}
+                name={data.name}
+                beginTime={data.begin_at}
+                endTime={data.end_at}
+                ></Card>
+            );
+            })}
         </div>
-    )
+    </div>
+  );
 }
 
-export default Tournaments
+export default TournamentsData;
