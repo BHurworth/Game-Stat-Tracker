8ad8d08cdb811efdda0cad0f67d577e27a242744
Ben Hurworth <ben.hurworth@gmail.com>
2021-04-15 15:25:36 +0100

    fix: remove unnecessary imports

--- client/src/components/Data/Tournaments.js
+++ client/src/components/Data/Tournaments.js
@@ -1,29 +1,29 @@
 import React from "react";
-import { useState } from "react";
-import axios from "axios";
 import Card from "./TournamentDataCards.js";
 
 function TournamentsData(props) {
-  console.log(props.data);
 
-
-  return (
-    <div id="tourn-container" className={props.className}>
-      <h1 className="tourn-title">Tournaments</h1>
+    return (
+      <div id="tourn-container" className={props.className}>
+        <h1 className="tourn-title">Tournaments</h1>
         <div id="tournament-cards">
-            {props.data.map((data) => {
+          {props.data.map((data) => {
+
             return (
-                <Card
-                // img={data.league.image_url}
+              <Card
+                img={data.league.image_url}
                 name={data.name}
                 beginTime={data.begin_at}
                 endTime={data.end_at}
-                ></Card>
+              ></Card>
             );
-            })}
+          })}
         </div>
-    </div>
-  );
+      </div>
+    );
 }
+
+
+
 
 export default TournamentsData;
