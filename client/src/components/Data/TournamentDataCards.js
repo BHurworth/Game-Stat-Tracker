8ccc4eb95cd6c0820257ad32a32427acc0264769
Ben Hurworth <ben.hurworth@gmail.com>
2021-04-15 16:00:20 +0100

    feat: convert date from ISO 8601 to standard GMT time

--- client/src/components/Data/TournamentDataCards.js
+++ client/src/components/Data/TournamentDataCards.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 function TournamentDataCards(props) {
+  var beginTimeUk = new Date(props.beginTime);
+  var endTimeUk = new Date(props.endTime);
+  beginTimeUk = beginTimeUk.toString();
+  endTimeUk = endTimeUk.toString();
+  beginTimeUk = beginTimeUk.split("+")[0]
+  endTimeUk = endTimeUk.split("+")[0];
+
+
   return (
-    <div>
+    <div id="tourn-card">
       <img src={props.img}></img>
-      <h1 className = "tourn-title">{props.name}</h1>
-      <h3 className = "tourn-starttime">Starts at: {props.beginTime}</h3>
-      <h3>Ends at: {props.endTime}</h3>
+      <h1 className="tourn-title">{props.name}</h1>
+      <h3 className="tourn-starttime">Starts at: {beginTimeUk}</h3>
+      <h3>Ends at: {endTimeUk}</h3>
     </div>
   );
 }
