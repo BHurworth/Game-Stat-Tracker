032b54d18c953c81e8f454b2c43f1c95625faec1
Ben Hurworth <ben.hurworth@gmail.com>
2021-04-17 13:22:45 +0100

    fix

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import csgoRoutes from "./routes/csgo.js";
-import steamRoutes from "./routes/steam.js"
+import steamRoutes from "./routes/steam.js";
 
 const app = express();
 app.use(cors());
@@ -14,29 +14,24 @@ app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
 app.use("/csgo", csgoRoutes);
 app.use("/steam", steamRoutes);
 
-if(process.env.NODE_ENV ==='production'){
-  app.use(express.static('client/build'))
-    app.use(express.static(path.join(__dirname, "client/build")));
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "client/build", "index.html"));
-    });
-}
+app.use(express.static("public"));
 
 const CONNECTION_URL =
   "mongodb+srv://benHur:1234@cluster0.oyyi2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(CONNECTION_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() =>
-    app.listen(PORT, () =>
-      console.log(`Connection is established and running on port: ${PORT}`)
-    )
-  )
-  .catch((err) => console.log(err.message));
+// mongoose
+//   .connect(CONNECTION_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() =>
+app.listen(PORT, () => {
+  console.log(`Connection is established and running on port: ${PORT}`);
+});
+//     )
+//   )
+//   .catch((err) => console.log(err.message));
 
-mongoose.set("useFindAndModify", false);
+// mongoose.set("useFindAndModify", false);
