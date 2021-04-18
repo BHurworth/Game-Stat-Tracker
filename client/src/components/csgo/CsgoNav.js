a49172466b487e4aa402801395d2850ec52cbd84
Ben Hurworth <ben.hurworth@gmail.com>
2021-04-18 13:04:35 +0100

    feat: add spinner before data loaded and add timeout test feature

--- client/src/components/csgo/CsgoNav.js
+++ client/src/components/csgo/CsgoNav.js
@@ -3,28 +3,30 @@ import Card from "../Navigation/Card.js";
 import SteamIdForm from "../Data/SteamIdForm.js";
 import Tournaments from "../Data/Tournaments.js";
 import axios from "axios";
+import Spinner from "../Misc/Spinner.js"
 import "../../App.css";
 
-
-
-
 export function CsgoNav() {
-
   const [dataLoaded, setdataLoaded] = useState(false);
 
   const getData = () => {
-    const url = "http://localhost:5000/csgo/tournaments";
+    const url =
+      "http://localhost:5000/csgo/tournaments";
 
-    axios.get(url).then((response) => {settournamentData(response.data)
-    setdataLoaded(true);});
-    
+      //"https://game-stat-tracker-server.herokuapp.com/csgo/tournaments";
+
+    axios.get(url).then((response) => {
+      settournamentData(response.data);
+      setdataLoaded(true);
+    });
   };
-
 
   const [mounted, setMounted] = useState(false);
 
   if (!mounted) {
+    // setTimeout(() => getData(), 1000);
     getData();
+
   }
 
   useEffect(() => {
@@ -39,38 +41,37 @@ export function CsgoNav() {
 
   const [tournamentData, settournamentData] = useState([{}]);
 
-  if(dataLoaded===false) {
-    return (<h1>LOADING</h1>)
+  if (dataLoaded === false) {
+    return <Spinner></Spinner>
+  } else if (dataLoaded === true) {
+    return (
+      <>
+        <div className="csgo-card-list">
+          <h1 onClick={() => toggleTab(1)}>Stat tracker</h1>
+          <h1
+            onClick={(e) => {
+              toggleTab(2);
+            }}
+          >
+            Tournaments
+          </h1>
+          <h1
+            onClick={(e) => {
+              toggleTab(3);
+            }}
+          >
+            Teams
+          </h1>
+        </div>
+        <SteamIdForm
+          className={toggleState === 1 ? "active-tab" : "tab"}
+          id = "trigger"
+        ></SteamIdForm>
+        <Tournaments
+          className={toggleState === 2 ? "active-tab" : "tab"}
+          data={tournamentData}
+        ></Tournaments>
+      </>
+    );
   }
-
-
-  else if (dataLoaded===true) {
-  return (
-    <>
-      <div className="csgo-card-list">
-        <h1 onClick={() => toggleTab(1)}>Stat tracker</h1>
-        <h1
-          onClick={(e) => {
-            toggleTab(2);
-          }}
-        >
-          Tournaments
-        </h1>
-        <h1
-          onClick={(e) => {
-            toggleTab(3);
-          }}
-        >
-          Teams
-        </h1>
-      </div>
-      <SteamIdForm
-        className={toggleState === 1 ? "active-tab" : "tab"}
-      ></SteamIdForm>
-      <Tournaments
-        className={toggleState === 2 ? "active-tab" : "tab"}
-        data={tournamentData}
-      ></Tournaments>
-    </>
-  );
-}}
+}
