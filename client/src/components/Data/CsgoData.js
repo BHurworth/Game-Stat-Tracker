3e030146f17d397325cccdc9725ca3687cbc882a
Ben Hurworth <ben.hurworth@gmail.com>
2021-04-18 20:59:06 +0100

    feat: add two objects containing list of numbers to be passed as props for data access in array

--- client/src/components/Data/CsgoData.js
+++ client/src/components/Data/CsgoData.js
@@ -1,21 +1,24 @@
-import React from 'react'
+import React from "react";
+import CsgoStatCard from "./CsgoStatCard.js";
 
-export function CsgoData({
-  personaName,
-  totalKills,
-  totalDeaths,
-  totalPlaytime,
-  totalKnifeKills,
-}) {
-  console.log(personaName);
+export function CsgoData(props) {
+  
+  const dataNumbers = {
+      "n1": 0,
+      "n2": 1,
+      "n3": 3,
+      "n4": 4,
+      "n5": 5,
+      "n6": 6,
+      "n7": 7,
+      "n8": 8,
+    }
+  const dataNumbers2 = {   "n1": 9, "n2": 10, "n3": 11, "n4": 12, "n5": 13, "n6": 14, "n7": 15, "n8": 16 }
+  
   return (
     <div className="csgo-data">
-   
+      <CsgoStatCard data={props} dataNumbers={dataNumbers}></CsgoStatCard>
+      <CsgoStatCard data={props} dataNumbers={dataNumbers2}></CsgoStatCard>
     </div>
   );
 }
-
-
-
-
-
