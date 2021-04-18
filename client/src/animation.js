00b8cc44225b07eaf25a79ba88fc05d9529d6176
Ben Hurworth <ben.hurworth@gmail.com>
2021-04-18 13:03:12 +0100

    feat: add animation js file

---
+++ client/src/animation.js
@@ -0,0 +1,10 @@
+// let t2 = gsap.timeline({
+//     scrollTrigger: {
+//       start:'top center',
+//       scrub: 1,
+//     }
+//   });
+//     t2.from(".csgo-card-list", { x: -800, opacity: 0, duration: 0.5 });
+ 
+
+  
