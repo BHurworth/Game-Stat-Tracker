bd36492eb5914e28dc3be4d26df9a8ce877fd051
Ben Hurworth <ben.hurworth@gmail.com>
2021-04-18 15:40:19 +0100

    feat: add CsgoProfile and CsgoData to SteamIdForm

--- client/src/components/Data/SteamIdForm.js
+++ client/src/components/Data/SteamIdForm.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import { Data } from "../Data/Data.js";
+import { Data } from "./CsgoData.js";
+import {CsgoProfile} from "../Data/CsgoProfile.js"
+import Spinner from "../Misc/Spinner.js";
+import {CsgoData} from "./CsgoData"
 
 function SteamIdForm(props) {
   const [steamIdData, setSteamIdData] = useState([{}]);
@@ -18,6 +21,7 @@ function SteamIdForm(props) {
   ]);
 
   const getDatas = (e, id) => {
+    setstatLoader(true);
     let csgoUrl = `http://localhost:5000/csgo/${id}`;                     
     //`https://game-stat-tracker-server.herokuapp.com/csgo/${id}`;
     let steamUrl = `http://localhost:5000/steam/${id}`
@@ -33,13 +37,21 @@ function SteamIdForm(props) {
         axios.spread((...responses) => {
           const responseOne = responses[0];
           const responseTwo = responses[1];
+          console.log(responseOne);
+          console.log(responseTwo);
           setCsgoData(responseOne.data.playerstats.stats);
           setSteamIdData(responseTwo.data.response.players);
+          setfindingStats(true);
+          setstatLoader(false);
         })
       )
       .catch((errors) => {
         console.log(errors);
       });
+
+      console.log(steamIdData[0].avatarfull);
+      
+
     e.preventDefault();
   };
 
@@ -47,38 +59,55 @@ function SteamIdForm(props) {
     steamId: 0,
   });
 
-  return (
-    <div id="section2" className={props.className}>
-      <h1>CS:GO personal stat checker</h1>
-  
-        <div class="search_wrap search_wrap_6">
-          <div class="search_box">
-            <input
-              name="steamID"
-              type="text"
-              class="input"
-              placeholder="Enter Steam64 ID"
-              onChange={(event) => {
-                const value = event.target.value;
-                setSteamId({ ...steamId, steamId: value });
-                console.log(steamId);
-              }}
-            ></input>
-            <div class="btn" onClick={(e) => getDatas(e, steamId.steamId)}>
-              <p>Enter</p>
-            </div>
-          </div>
-        </div>
+    const [findingStats, setfindingStats] = useState(false);
+    const [statLoader, setstatLoader] = useState(false);
 
-      <Data
+
+
+return (
+  <div id="section2" className={props.className}>
+    <h1>CS:GO personal stat checker</h1>
+
+    <div class="search_wrap search_wrap_6">
+      <div class="search_box">
+        <input
+          name="steamID"
+          type="text"
+          class="input"
+          placeholder="Enter Steam64 ID"
+          onChange={(event) => {
+            const value = event.target.value;
+            setSteamId({ ...steamId, steamId: value });
+            console.log(steamId);
+          }}
+        ></input>
+        <div class="btn" onClick={(e) => getDatas(e, steamId.steamId)}>
+          <p>Enter</p>
+        </div>
+      </div>
+    </div>
+
+    {/* <Data
         personaName={steamIdData[0].personaname}
         totalKills={csgoData[0].value}
         totalDeaths={csgoData[1].value}
         totalPlaytime={csgoData[2].value}
         totalKnifeKills={csgoData[9].value}
-      />
-    </div>
-  );
-}
+      /> */}
+    {statLoader ? <Spinner size="small"></Spinner> : null}
+    {findingStats ? (
+      <CsgoProfile
+        profilePicture={steamIdData[0].avatarfull}
+        personaName={steamIdData[0].personaname}
+        playtime={csgoData[2].value}
+        profileUrl={steamIdData[0].profileurl}
+      ></CsgoProfile>
+    ) : null}
+    {findingStats ? (<CsgoData></CsgoData>):null}
+  </div>
+);
+  }
+  
+
 
 export default SteamIdForm;
