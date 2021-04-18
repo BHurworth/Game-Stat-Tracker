a615427b7cf10e72c7bbb229c7020dcae65593dd
Ben Hurworth <ben.hurworth@gmail.com>
2021-04-18 15:37:55 +0100

    feat: add component to show user's csgo stats

---
+++ client/src/components/Data/CsgoData.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export function CsgoData({
+  personaName,
+  totalKills,
+  totalDeaths,
+  totalPlaytime,
+  totalKnifeKills,
+}) {
+  console.log(personaName);
+  return (
+    <div className="csgo-data">
+   
+    </div>
+  );
+}
+
+
+
+
+
