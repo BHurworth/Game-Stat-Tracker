082c85c3516b231b40c94ba16ca11449d7c03892
Ben Hurworth <ben.hurworth@gmail.com>
2021-04-18 13:03:58 +0100

    feat: add Spinner component

---
+++ client/src/components/Misc/Spinner.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Spinner() {
+  return (
+    <div className="spinner-wrapper">
+      <div class="loader">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  );
+}
+
+export default Spinner;
