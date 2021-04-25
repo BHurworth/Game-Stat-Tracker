c985a1aba81d27257f5ff0a6629d3501a9ed49fd
Ben Hurworth <ben.hurworth@gmail.com>
2021-04-25 13:54:11 +0100

    fix: add test console.log line

--- server/controllers/csgoTournaments.js
+++ server/controllers/csgoTournaments.js
@@ -1,18 +1,14 @@
 import fetch from "node-fetch";
 
 export const getCsgoTournaments = async (req, res) => {
-
   try {
-    console.log("yo");
     const api_url = `https://api.pandascore.co/csgo/tournaments/upcoming?token=L-KSCBAZ-Cxv71k544VKEqbfcH7t1ZNUrdBFThn-P8fFEG_EFlY`;
 
     const profileResponse = await fetch(api_url);
     const profile = await profileResponse.json();
 
-   
     res.send(profile);
     res.status(200);
-
   } catch (error) {
     console.log("Fuck");
     res.status(404).json({ message: error.message });
