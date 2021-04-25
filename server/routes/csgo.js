39d8788e43a781d779eae2c71932b9d6be636c62
Ben Hurworth <ben.hurworth@gmail.com>
2021-04-25 13:54:35 +0100

    feat: add /news route and re order to fix bug

--- server/routes/csgo.js
+++ server/routes/csgo.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { getCsgoStats } from "../controllers/csgo.js";
-import {getCsgoTournaments} from "../controllers/csgoTournaments.js"
+import { getCsgoTournaments } from "../controllers/csgoTournaments.js";
+import { getCsgoNews } from "../controllers/csgoNews.js";
 
 const router = express.Router();
 
 router.get("/tournaments", getCsgoTournaments);
+
+router.get("/news", getCsgoNews);
 
 router.get("/:id", getCsgoStats);
 
