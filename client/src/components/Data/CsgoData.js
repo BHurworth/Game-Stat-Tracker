e0ad988fc130307b156d539158852a9b323ecd66
Ben Hurworth <ben.hurworth@gmail.com>
2021-05-05 21:14:58 +0100

    feat: uncomment 'CsgoStatCard'(s) in CsgoData, so user stats are displayed on fetch

--- client/src/components/Data/CsgoData.js
+++ client/src/components/Data/CsgoData.js
@@ -2,19 +2,27 @@ import React from "react";
 import CsgoStatCard from "./CsgoStatCard.js";
 
 export function CsgoData(props) {
-  
   const dataNumbers = {
-      "n1": 0,
-      "n2": 1,
-      "n3": 3,
-      "n4": 4,
-      "n5": 5,
-      "n6": 6,
-      "n7": 7,
-      "n8": 8,
-    }
-  const dataNumbers2 = {   "n1": 9, "n2": 10, "n3": 11, "n4": 12, "n5": 13, "n6": 14, "n7": 15, "n8": 16 }
-  
+    n1: 0,
+    n2: 1,
+    n3: 3,
+    n4: 4,
+    n5: 5,
+    n6: 6,
+    n7: 7,
+    n8: 8,
+  };
+  const dataNumbers2 = {
+    n1: 9,
+    n2: 10,
+    n3: 11,
+    n4: 12,
+    n5: 13,
+    n6: 14,
+    n7: 15,
+    n8: 16,
+  };
+
   return (
     <div className="csgo-data">
       <CsgoStatCard
